2afdfa9ea304726a69e9cae588c5ca7925ac0366
Victor Vinicius <victormafiavinicius@gmail.com>
2024-03-01 09:31:44 -0300

    upcasting and downcasting

---
+++ Course/Entities/SavingsAccount.cs
@@ -0,0 +1,29 @@
+﻿ 
+
+namespace Course.Entities
+{
+    internal class SavingsAccount : Account
+    {
+        public double InterestRate { get;  set; }
+
+        public SavingsAccount()
+        {
+
+        } 
+        public SavingsAccount(int number, string holder, double balance, double interestRate) : base(number, holder, balance)
+        {
+            InterestRate = interestRate;
+        }
+
+        public void UpdateBalance()
+        {
+            Balance += Balance * InterestRate;
+        } 
+        // reimplementamos na sub classe a operação que já existia na super classe. Assim dá um comportamento diferente e é aceita.
+        public override void Withdraw(double amount)
+        {
+            base.Withdraw(amount); // com o base conseguimos reaproveitar a operação da supe classe e ainda posso acrescentar outras coisas.
+            Balance -= 2.0;
+        } 
+    }
+}
