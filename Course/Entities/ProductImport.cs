ece612a7ac9e4eaf2ffefdc8fdba6059c4a875b4
Victor Vinicius <victormafiavinicius@gmail.com>
2024-03-01 11:55:09 -0300

    Novas aprendizagens

---
+++ Course/Entities/ProductImport.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Course.Entities
+{
+    internal class ProductImport
+    {
+        public string Name { get; set; }
+        public double Price { get; set; }
+
+        public ProductImport()
+        {
+
+        } 
+        public ProductImport(string name, double price)
+        {
+            Name = name;
+            Price = price;
+        }
+
+        public virtual string PriceTag()
+        {
+            return Name 
+                + " $ " 
+                + Price.ToString("F2", CultureInfo.InvariantCulture);
+        }
+
+    }
+}
