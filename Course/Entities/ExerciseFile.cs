f80768a3e6b220cd4c6853e8791160e18dee41e4
Victor Vinicius <victormafiavinicius@gmail.com>
2024-03-15 10:35:29 -0300

    Aprendendo Services

---
+++ Course/Entities/ExerciseFile.cs
@@ -0,0 +1,22 @@
+﻿namespace Course.Entities
+{
+    class ExerciseFile
+    {
+
+        public string Name { get; set; }
+        public double Price { get; set; }
+        public int Quantity { get; set; }
+
+        public ExerciseFile(string name, double price, int quantity)
+        {
+            Name = name;
+            Price = price;
+            Quantity = quantity;
+        }
+
+        public double Total()
+        {
+            return Price * Quantity;
+        }
+    }
+}
