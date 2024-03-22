fc9d3ae7fd36d1272b7881a1574c795358c9052d
Victor Vinicius <victormafiavinicius@gmail.com>
2024-03-22 09:39:07 -0300

    Exercicio usando extensões e DATE TIME.

---
+++ Course/ProgramDateTime.cs
@@ -0,0 +1,16 @@
+﻿using System; 
+
+namespace Course
+{
+    internal class ProgramDateTime
+    {
+        static void MainDT(string[] args)
+        {
+            DateTime dt = new DateTime(2018, 11, 16, 8, 10, 45);
+            Console.WriteLine(dt.ElapsedTime());
+
+            string s1 = "Good morming dear students!";
+            Console.WriteLine(s1.Cut(10));
+        }
+    }
+}
