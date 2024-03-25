ba3981eacc5f47d6c3726b4cdcb4e9ad45739b2a
Victor Vinicius <victormafiavinicius@gmail.com>
2024-03-25 10:46:56 -0300

    Utilizando Action com Foreach e Utilizando Func com Select.

---
+++ Course/ProgramActionForEach.cs
@@ -0,0 +1,26 @@
+﻿using Course.Entities;
+using Course.Services;
+using System;
+using System.Globalization;
+
+namespace Course
+{ 
+    internal class ProgramActionForEach
+    {
+        static void MainAF(string[] args)
+        {
+            List<ProductPredicate> list = new List<ProductPredicate>();
+
+            list.Add(new ProductPredicate("Tv", 900.00));
+            list.Add(new ProductPredicate("Mouse", 50.00));
+            list.Add(new ProductPredicate("Tablet", 350.50));
+            list.Add(new ProductPredicate("HD Case", 80.90));
+
+            list.ForEach(p => { p.Price += p.Price * 0.1; }); 
+            foreach (ProductPredicate p in list)
+            {
+                Console.WriteLine(p);
+            }
+        }  
+    }
+}
---
+++ Course/ProgramFuncSelect.cs
@@ -0,0 +1,29 @@
+﻿using Course.Entities; 
+using System.Collections.Generic;
+using System;
+using System.Linq;
+
+namespace Course
+{ 
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<ProductPredicate> list = new List<ProductPredicate>();
+
+            list.Add(new ProductPredicate("Tv", 900.00));
+            list.Add(new ProductPredicate("Mouse", 50.00));
+            list.Add(new ProductPredicate("Tablet", 350.50));
+            list.Add(new ProductPredicate("HD Case", 80.90));
+
+            Func<ProductPredicate, string> func = NameUpper;
+
+            List<string> result = list.Select(func).ToList();
+
+            foreach (string s in result)
+            {
+                Console.WriteLine(s);
+            }
+        } 
+    }
+}
