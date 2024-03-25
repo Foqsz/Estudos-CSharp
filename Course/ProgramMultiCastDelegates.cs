bdf640c7c57ffcb7fc017df498e1e246d1497b95
Victor Vinicius <victormafiavinicius@gmail.com>
2024-03-25 09:22:53 -0300

    multi cast delegates

---
+++ Course/ProgramMultiCastDelegates.cs
@@ -0,0 +1,22 @@
+﻿using Course.Entities;
+using Course.Services;
+using System;
+using System.Globalization;
+
+namespace Course
+{
+    delegate double BinaryNumaricOperationn(double n1, double n2);
+    internal class ProgramMultiCastDelegates
+    {
+        static void MainCZ(string[] args)
+        {
+            double a = 10;
+            double b = 12; 
+
+            BinaryNumaricOperationn op = CalculationDelegate.Max;
+
+            double result = op.Invoke(a, b);
+            Console.WriteLine(result);
+        } 
+    }
+}
