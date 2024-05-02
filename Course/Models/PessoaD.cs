aec518bc21a19cf1583f89a48c267f32f19bd2dd
Victor Vinicius <victormafiavinicius@gmail.com>
2024-05-02 09:07:33 -0300

    Iniciando um obj

---
+++ Course/Models/PessoaD.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Course.Models
+{
+    internal class PessoaD
+    {
+        public string Nome { get; set; }
+        public int Idade { get; set; }
+        public string Cidade { get; set; }
+        public string Estado { get; set; }
+
+        public void Apresentar()
+        {
+            Console.WriteLine($"Olá meu nome é {Nome}, e tenho {Idade} anos. Moro em {Cidade} na {Estado}.");
+        }
+    }
+}
---
+++ Course/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Course.Models;
+
+namespace Course
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            PessoaD PessoaP1 = new PessoaD(); //Iniciar um objeto.
+
+            PessoaP1.Nome = "Victor";
+            PessoaP1.Idade = 20;
+            PessoaP1.Cidade = "Santa Rita";
+            PessoaP1.Estado = "Paraíba";
+
+            PessoaP1.Apresentar();
+        }
+    }
+}
