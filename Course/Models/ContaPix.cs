835d9bab6b50f3cf0657e7455f3348fdd6c7c184
Victor Vinicius <victormafiavinicius@gmail.com>
2024-05-02 11:27:33 -0300

    Revendo método Abstract

---
+++ Course/Models/ContaPix.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Course.Models
+{
+    public abstract class ContaPix
+    {
+        protected decimal saldo;
+        public abstract void Creditar(decimal valor);
+
+        public void ExibirSaldo()
+        {
+            Console.WriteLine($"Seu saldo é {saldo}.");
+        }
+    }
+}
---
+++ Course/ProgramHerancaPolimorfismoAbstract.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using Course.Models;
+
+namespace Course
+{
+    internal class ProgramHerancaPolimorfismoAbstract
+    {
+        static void MainxAbsx(string[] args)
+        {
+            PessoaD PessoaP1 = new PessoaD(); //Iniciar um objeto.
+            PessoaP1.Nome = "Victor";
+            PessoaP1.Idade = 20;
+            PessoaP1.Apresentar();
+            Console.WriteLine("-------------------------------");
+
+            Student aluno = new Student();
+            aluno.Nome = "Vinicius";
+            aluno.Idade = 20;
+            aluno.Email = "foqs@gmail.com";
+            aluno.Nota = 9;
+            aluno.Apresentar();
+            Console.WriteLine("-------------------------------");
+
+            Teatcher professor = new Teatcher();
+            professor.Nome = "Pedro";
+            professor.Idade = 35;
+            professor.Salario = 3700;
+            professor.Apresentar();
+            Console.WriteLine("-------------------------------");
+
+            ContaPixCorrente c = new ContaPixCorrente();
+            c.Creditar(100);
+            c.ExibirSaldo();
+            Console.WriteLine("-------------------------------");
+
+            //--------------------------------------------//
+
+            Console.WriteLine("-------------------------------");
+            NewPix pix = new NewPix(654, 1000);
+
+            pix.ExibirSaldo();
+            pix.Sacar(5000);
+            pix.ExibirSaldo();
+
+
+        }
+    }
+}
