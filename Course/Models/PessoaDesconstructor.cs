83c8fcda766db2d325faaafefe86396b7dc98a9e
Victor Vinicius <victormafiavinicius@gmail.com>
2024-05-10 11:14:51 -0300

    Aprendendo sobre deconstrução de um objeto

---
+++ Course/Models/PessoaDesconstructor.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Course.Models
+{
+    internal class PessoaDesconstructor
+    {
+        public string Nome { get; set; }
+        public string sobreNome { get; set; }
+
+        public PessoaDesconstructor()
+        {
+
+        }
+
+        public PessoaDesconstructor(string nome, string sobrenome)
+        {
+            Nome = nome;
+            sobreNome = sobrenome;
+        }
+
+        public void Deconstruct(out string nome, out string sobrenome)
+        {
+            nome = Nome;
+            sobrenome = sobreNome;
+        }
+    }
+}
