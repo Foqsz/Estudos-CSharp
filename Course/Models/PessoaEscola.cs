3f6f77c269da71070fc764914f9917633d1bbd87
Victor Vinicius <victormafiavinicius@gmail.com>
2024-05-10 10:56:01 -0300

    Aprendendo sobre Tupla e também leitura de arquivo utilizando método tupla, com try-catch.

---
+++ Course/Models/PessoaEscola.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Course.Models
+{
+    internal class PessoaEscola
+    {
+        public string Nome { get; set; }
+        public List<PessoaEscola> Alunos { get; set; }
+
+        public void AdicionarAluno(PessoaEscola pessoa)
+        {
+            Alunos.Add(pessoa);
+        }
+
+        public int ObterQuantidadeDeAlunos()
+        {
+            int quant = Alunos.Count;
+            return quant;
+        }
+
+        public bool RemoverAluno(PessoaEscola pessoa)
+        {
+            return Alunos.Remove(pessoa);
+        }
+
+        public void ListarAlunos()
+        {
+            Console.WriteLine($"Alunos do curso de {Nome}");
+            for (int count = 0; count < Alunos.Count; count++)
+            {
+                string texto = "N° " + count + " " + Nome;
+                Console.WriteLine(texto);
+            }
+        }
+    }
+}
---
+++ Course/ProgramTUPLA.cs
@@ -0,0 +1,30 @@
+﻿using Course.Models;
+using System;
+using System.Globalization;
+
+namespace Course
+{
+    internal class ProgramTUPLA
+    { 
+        static void MainTupla(string[] args)
+        {
+            LeituraArquivo leituraArquivo = new LeituraArquivo();
+
+            var (sucesso, linhasArquivo, quantLinhas) = leituraArquivo.LerArquivo("C:\\Users\\vinic\\Documents\\Projetos Estudo\\Course\\Course\\Arquives\\Arquivo.txt");
+             
+            if (sucesso)
+            {
+
+                Console.WriteLine("Quantidade linhas do arquivo: " + quantLinhas);
+                foreach (string linha in linhasArquivo)
+                {
+                    Console.WriteLine(linha);
+                }
+            }
+            else
+            {
+                Console.WriteLine("Não foi possivel ler o arquivo");
+            }
+        }
+    }
+}
